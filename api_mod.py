9f5c5b6efc28de2fc1b51bfa393661f9c1232d00
JaylenLuc <91278747+JaylenLuc@users.noreply.github.com>
2022-07-09 21:03:08 -0700

    Add files via upload

--- api_mod.py
+++ api_mod.py
@@ -1,4 +1,7 @@
 import requests
+import time
+import json
+
 class color:
    PURPLE = '\033[95m'
    CYAN = '\033[96m'
@@ -10,37 +13,47 @@ class color:
    BOLD = '\033[1m'
    UNDERLINE = '\033[4m'
    END = '\033[0m'
+   
 
 class server_fetch:
+    fetch_time = 0
+    response = ''
 
+    def __init__(self):
+        server_fetch.response = requests.get('https://restcountries.com/v3.1/all').json()
+        #server_fetch.response = json.loads(server_fetch.response.read())
+
+    @staticmethod 
+    def rettime():
+        return server_fetch.server_fetch.rettime
     def fetch(self,code):
-        response = requests.get(f'https://restcountries.com/v3.1/alpha/{code}').json()
-        return response[0]
+        server_fetch.response = requests.get(f'https://restcountries.com/v3.1/alpha/{code}').json()
+        return server_fetch.response[0]
 
 
     def fetch_assign(self,code):
-        response = requests.get(f'https://restcountries.com/v3.1/alpha/{code}').json()
-        self.native_name = f'Autochthonous Name: '+list(response[0]['name']['nativeName'].items())[0][1]['official'] #get native language name
-        self.name = f'Anglophone Name: '+response[0]['name']['common'] #get name
-        self.capital = f'Administrative Center: '+response[0]['capital'][0] #get capital
-        self.subregion = f'Subregion: '+response[0]['subregion'] #get region
-        self.lang = f'Lingua Franca: '+list(response[0]['languages'].items())[0][1] #get lang
-        self.pop = f'Population: '+ str(response[0]['population']) #get pop
-        self.time = f'Timezone (UTC): ' + response[0]['timezones'][0] #timezone
+        server_fetch.response = requests.get(f'https://restcountries.com/v3.1/alpha/{code}').json()
+        self.native_name = f'Autochthonous Name: '+list(server_fetch.response[0]['name']['nativeName'].items())[0][1]['official'] #get native language name
+        self.name = f'Anglophone Name: '+server_fetch.response[0]['name']['common'] #get name
+        self.capital = f'Administrative Center: '+server_fetch.response[0]['capital'][0] #get capital
+        self.subregion = f'Subregion: '+server_fetch.response[0]['subregion'] #get region
+        self.lang = f'Lingua Franca: '+list(server_fetch.response[0]['languages'].items())[0][1] #get lang
+        self.pop = f'Population: '+ str(server_fetch.response[0]['population']) #get pop
+        self.time = f'Timezone (UTC): ' + server_fetch.response[0]['timezones'][0] #timezone
         try:
-            self.gini = f'Gini Income Inequality Index: '+str(list(response[0]['gini'].values())[0])+ ' per ' + str(list(response[0]['gini'].keys())[0]) #gini index
+            self.gini = f'Gini Income Inequality Index: '+str(list(server_fetch.response[0]['gini'].values())[0])+ ' per ' + str(list(server_fetch.response[0]['gini'].keys())[0]) #gini index
         except(KeyError):
             self.gini = f'Gini Income Inequality Index: '+ 'N/A'
         
         try:
-            self.money = f'Fiat Currency: '+ str(list(response[0]['currencies'].items())[0][1]['name'])+ ' '+ str(list(response[0]['currencies'].items())[0][1]['symbol']) #currrency #currencu symb
+            self.money = f'Fiat Currency: '+ str(list(server_fetch.response[0]['currencies'].items())[0][1]['name'])+ ' '+ str(list(server_fetch.response[0]['currencies'].items())[0][1]['symbol']) #currrency #currencu symb
 
         except(KeyError):
-            self.money = f'Fiat Currency: '+ str(list(response[0]['currencies'].items())[0][1]['name']) #currrency #currencu symb
+            self.money = f'Fiat Currency: '+ str(list(server_fetch.response[0]['currencies'].items())[0][1]['name']) #currrency #currencu symb
 
     def raw_print(self,code):
-        response = requests.get(f'https://restcountries.com/v3.1/alpha/{code}').json()
-        print(response)
+        server_fetch.response = requests.get(f'https://restcountries.com/v3.1/alpha/{code}').json()
+        print(server_fetch.response)
         
         #next order of business:: get the pop and try to do the images of flag
     def printer(self):
@@ -58,63 +71,85 @@ class server_fetch:
     def in_fetch(self):
         self.income  = requests.get('http://api.worldbank.org/v2/country?format=json').json()
     
+    def get_income(self,code):
+        return requests.get(f'http://api.worldbank.org/v2/country/{code}?format=json').json()
+
     def form_str(self,code):
-        income = requests.get(f'http://api.worldbank.org/v2/country/{code}?format=json').json()
-        response = requests.get(f'https://restcountries.com/v3.1/alpha/{code}').json()
-        try:
-            color = ''
-            color_end = ''
-            style = ''
-            if int(list(response[0]['gini'].values())[0]) < 30:
-                style = '<style>pr {color:#008000; display:inline;}</style> '
-                color = '<pr>'
-                color_end = '</pr>'
-            elif int(list(response[0]['gini'].values())[0]) >= 30 and int(list(response[0]['gini'].values())[0]) < 45:
-                style = '<style>pr {color:#FFA500; display:inline;}</style> '
-                color = '<pr>'
-                color_end = '</pr>'
-            elif int(list(response[0]['gini'].values())[0]) >= 45 :
-                style = '<style>pr {color:#FF0000; display:inline;}</style> '
-                color = '<pr>'
-                color_end = '</pr>'
-            self.gini = style+ f'<b>Gini Income Inequality Index:</b> '+color+str(list(response[0]['gini'].values())[0])+ color_end+' per ' + str(list(response[0]['gini'].keys())[0]) #gini index
-        except(KeyError):
-            self.gini =style+ f'<b>Gini Income Inequality Index:</b> '+ 'N/A'
-        
-        try:
-            self.money = f'<b>Fiat Currency:</b> '+ str(list(response[0]['currencies'].items())[0][1]['name'])+ ' '+ str(list(response[0]['currencies'].items())[0][1]['symbol']) #currrency #currencu symb
+        #most  ofthe time is taken here
 
-        except(KeyError):
-            self.money = f'<b>Fiat Currency:</b> '+ str(list(response[0]['currencies'].items())[0][1]['name']) #currrency #currencu symb
-        t = 0
-        lang = ''
-        name= ''
+        start = time.time()
+        income = self.get_income(code)
+        #response = requests.get(f'https://restcountries.com/v3.1/alpha/{code}').json()
+        end = time.time()
+        for i in range(250):
+            #print(i)
+            if server_fetch.response[i]['cca3'] == code:
 
-        for k,v in response[0]['languages'].items():
-            if t == 1: lang += '<b>Other languages:</b> '
-            if not t == 0: lang += f'{v} / '
-            t+= 1
-        lang = lang[0:-2]
-        if code == 'ZWE': name = list(response[0]['name']['nativeName'].items())[0][1]['official']
-        else:
+                try:
+                    color = ''
+                    color_end = ''
+                    style = ''
+                    gin = float(list(server_fetch.response[i]['gini'].values())[0])
+                    if gin < 30:
+                        style = '<style>pr {color:#008000; display:inline;}</style> '
+                        color = '<pr>'
+                        color_end = '</pr>'
+                    elif gin >= 30 and gin < 45:
+                        style = '<style>pr {color:#FFA500; display:inline;}</style> '
+                        color = '<pr>'
+                        color_end = '</pr>'
+                    elif gin >= 45 :
+                        style = '<style>pr {color:#FF0000; display:inline;}</style> '
+                        color = '<pr>'
+                        color_end = '</pr>'
+                    self.gini = style+ f'<b>Gini Income Inequality Index:</b> '+color+str(gin)+ color_end+' per ' + str(list(server_fetch.response[i]['gini'].keys())[0]) #gini index
+                except(KeyError):
+                    self.gini =style+ f'<b>Gini Income Inequality Index:</b> '+ 'N/A'
+                
+                try:
+                    y = str(list(server_fetch.response[i]['currencies'].items())[0][1]['name'])
+                    self.money = f'<b>Fiat Currency:</b> '+ y+ ' '+ str(list(server_fetch.response[i]['currencies'].items())[0][1]['symbol']) #currrency #currencu symb
 
-            for k,v in response[0]['name']['nativeName'].items():
-                name += v['official'] + ' / '
+                except(KeyError):
+                    self.money = f'<b>Fiat Currency:</b> '+ y #currrency #currencu symb
+                t = 0
+                lang = ''
+                name= ''
 
-            name = name[0:-2]
-        try:
-            income = income[1][0]['incomeLevel']['value']
-        except:
-            income = 'N\A'
-        if len(lang) != 0: lang += '</br>'
-        return f'<b>Autochthonous Name:</b> '+ name + '<br>'\
-                +f'<b>Anglophone Name:</b> '+response[0]['name']['common']+ '<br>'\
-                    +str(f'<b>Administrative Center:</b> '+response[0]['capital'][0]) + '<br>'\
-                        +str(f'<b>Subregion:</b> '+response[0]['subregion'])+ '<br>'\
-                            +  str( f'<b>Lingua Franca:</b> '+list(response[0]['languages'].items())[0][1])+ '<br>'\
-                                +lang + str(f'<b>Population:</b> '+ str(response[0]['population'])) + '<br>'\
-                                        +str(f'<b>Timezone (UTC):</b> ' + response[0]['timezones'][0]) + '<br>'+self.gini + '<br>'+self.money + '<br>'\
-                                            +'<b>Income Level:</b> '+income +  '</div>'
+                for k,v in server_fetch.response[i]['languages'].items():
+                    if t == 1: lang += '<b>Other languages:</b> '
+                    if not t == 0: lang += f'{v} / '
+                    t+= 1
+                lang = lang[0:-2]
+                if code == 'ZWE': name = list(server_fetch.response[i]['name']['nativeName'].items())[0][1]['official']
+                else:
+
+                    for k,v in server_fetch.response[i]['name']['nativeName'].items():
+                        name += v['official'] + ' / '
+
+                    name = name[0:-2]
+                try:
+                    income = income[1][0]['incomeLevel']['value']
+                except:
+                    income = 'N\A'
+                
+                if len(lang) != 0: lang += '</br>'
+                #'<img src="data:image/jpeg;base64,{}">'
+                flag = server_fetch.response[i]['flags']['png']
+                ret = f'<b>Autochthonous Name:</b> '+ name + '<br>'\
+                        +f'<b>Anglophone Name:</b> '+server_fetch.response[i]['name']['common']+ '<br>'+f'<img src="{flag}" style="width:80px;height:60px;">'+'<br>'\
+                            +str(f'<b>Administrative Center:</b> '+server_fetch.response[i]['capital'][0]) + '<br>'\
+                                +str(f'<b>Subregion:</b> '+server_fetch.response[i]['subregion'])+ '<br>'\
+                                    +  str( f'<b>Lingua Franca:</b> '+list(server_fetch.response[i]['languages'].items())[0][1])+ '<br>'\
+                                        +lang + str(f'<b>Population:</b> '+ str(server_fetch.response[i]['population'])) + '<br>'\
+                                                +str(f'<b>Timezone (UTC):</b> ' + server_fetch.response[i]['timezones'][0]) + '<br>'+self.gini + '<br>'+self.money + '<br>'\
+                                                    +'<b>Income Level:</b> '+income +  '</div>'
+                
+                server_fetch.fetch_time += end-start
+                break
+            
+        return ret
+
 
 
 
--- main_map.py
+++ main_map.py
@@ -1,10 +1,13 @@
 #interactive map
-#jaylen ho luc, Neal Lowry
+#jaylen ho luc
 import folium
 import pandas as pd
 import json
-import requests
-import api_mod
+import api_mod as api
+import time
+from folium.plugins import MousePosition
+from alive_progress import alive_bar; import time
+start1 = time.time()
 
 main_map = folium.Map(
     location=[39.9042, 116.4074],   
@@ -12,8 +15,25 @@ main_map = folium.Map(
     zoom_start = 3
 )
 
+formatter_lat = "function(num) {return L.Util.formatNum(num, 3) + ' º '+'N';};"
+formatter_long = "function(num) {return L.Util.formatNum(num, 3) + ' º '+'E';};"
+
+
+MousePosition(
+    position="topright",
+    separator=" | ",
+    empty_string="NaN",
+    lng_first=True,
+    num_digits=20,
+    prefix="Coordinates:",
+    lat_formatter=formatter_lat,
+    lng_formatter=formatter_long,
+).add_to(main_map)
+
 add = '/tile/{z}/{y}/{x}'
+
 #add more map textures later
+
 maps = dict(Grey='https://server.arcgisonline.com/arcgis/rest/services/Canvas/World_Light_Gray_Base/MapServer',
             Divisional='https://server.arcgisonline.com/ArcGIS/rest/services/Specialty/DeLorme_World_Base_Map/MapServer',
             Topographical='https://services.arcgisonline.com/arcgis/rest/services/World_Topo_Map/MapServer',
@@ -59,15 +79,20 @@ Euro_Hispanic = folium.FeatureGroup(name='Euro-Hispanic Occupied South-West Amer
 Gran_colombia = folium.FeatureGroup(name='Gran_colombia')
 Caribbean = folium.FeatureGroup(name='Caribbean')
 
+#class to fetch api information
+country_object = api.server_fetch()
+#country_object.in_fetch()
+
 class nation:
     classification = []
+    style_func_time = 0
     Sinosphere = ['CHN','PRK','KOR','JPN','TWN','SGP','VNM']
     Indo_China = ['LAO','THA','MMR','KHM','PHL','MYS','IDN','TLS']
     Hindustan = ['IND','PAK','BGD','LKA']
     Middle_East = ['IRN','IRQ','SYR','SYR','ISR','PSE','LBN',\
         'SAU','YEM','OMN','ARE','QAT','BHR','KWT','AFG','JOR']
     Turkistan = ['TKM','UZB','KAZ','KGZ','TJK','MNG']
-    The_West = ['MLT','ESP','PRT','FRA','BEL','NLD','GBR','IRL',\
+    The_West = ['GIB','SMR','VAT','MLT','ESP','PRT','FRA','BEL','NLD','GBR','IRL',\
         'DEU','LUX','ITA','AND','CHE','AUT','DNK','NOR','SWE','FIN','EST','FRO','ISL',\
         'GRL','MCO','AUS','NZL']
     The_Orthodoxy = ['POL','LTU','LVA','BLR','UKR','CZE','SVK','HUN'\
@@ -108,10 +133,19 @@ class nation:
             'fillOpacity' : .2
         }
         #included in the tooltip popup:: 
-        geo = folium.GeoJson(
+        #print(i['properties']['ISO_A3'])
+        st = time.time()
+        #print(i['properties']['ISO_A3'])
+        ht = country_object.form_str(i['properties']['ISO_A3'])
+
+        en = time.time()
+        
+        html = f'<style>pz {{color:{colors}; display:inline;}}</style>'+f'<div style="white-space: normal"><b>Civilization</b>: \
+            <pz>{namez}</pz><br>{ht}'
+        geo = folium.features.GeoJson(
             i,
-            tooltip = folium.GeoJsonTooltip(fields=('ADMIN','ISO_A3',), aliases=('Nation-State','ALPHA3',)),
-            #tooltip = folium.Tooltip(),
+            #tooltip = folium.GeoJsonTooltip(fields=('ADMIN','ISO_A3',), aliases=('Nation-State','ALPHA3',)),
+            tooltip= folium.Tooltip(text=folium.Html(html, script=True,width=400).render()),
             show = True,
             style_function=lambda x:bordersStyle)
         popup = folium.Popup('Hi')
@@ -121,71 +155,181 @@ class nation:
 
         #.add_to(main_map)
         #nation.classification.append(new_country)
-
+ugh = time.time()
 with open(r'C:\Users\Jaylen\Desktop\projects\countries.geojson') as open_f: #replace file path of the countries geojson file
     country_r = json.loads(open_f.read())
-'''
-print(len(nation.classification))
-for i in range(6):
-    print(nation.classification[i])
-'''
+ughe = time.time()
+print(f'open file: {ughe-ugh}')
+func_time = 0
+start2 = time.time()
+
+#each country code has a value which is the style function maybe maybe not so much of a pain in the ass
+
+#make this a lot shorter and cleawner and less bopilerplate by making a dicitonary 
+#optimize style_func
+
 for i in country_r['features']:
     #sinosphere
-    if i['properties']['ISO_A3'] in nation.Sinosphere : nation.style_func('#D12601','#D12601', i,"Sinosphere",sino)
+    if i['properties']['ISO_A3'] in nation.Sinosphere : 
+        
+        y = time.time()
+        nation.style_func('#D12601','#D12601', i,"Sinosphere",sino)
+        x = time.time()
+        func_time += x-y
     #indo-china
-    elif i['properties']['ISO_A3'] in nation.Indo_China: nation.style_func('#cc8899','#cc8899', i,"Indo China",indo)
+    elif i['properties']['ISO_A3'] in nation.Indo_China: 
+        y = time.time()
+        nation.style_func('#cc8899','#cc8899', i,"Indo China",indo)
+        x = time.time()
+        func_time += x-y
     #Hindustan
-    elif i['properties']['ISO_A3'] in nation.Hindustan: nation.style_func('#654321','#654321', i,"Hindustan",hindu)
+    elif i['properties']['ISO_A3'] in nation.Hindustan: 
+        y = time.time()
+        nation.style_func('#654321','#654321', i,"Hindustan",hindu)
+        x = time.time()
+        func_time += x-y
     #Middle East
-    elif i['properties']['ISO_A3'] in nation.Middle_East: nation.style_func('#009000','#009000', i,"Middle East",ME)
+    elif i['properties']['ISO_A3'] in nation.Middle_East:
+        y = time.time()
+        nation.style_func('#009000','#009000', i,"Middle East",ME)
+        x = time.time()
+        func_time += x-y
     #Turkistan
-    elif i['properties']['ISO_A3'] in nation.Turkistan: nation.style_func('#09EBEE','#09EBEE', i,"Turkistan",turkistan)
+    elif i['properties']['ISO_A3'] in nation.Turkistan: 
+        y = time.time()
+        nation.style_func('#09EBEE','#09EBEE', i,"Turkistan",turkistan)
+        x = time.time()
+        func_time += x-y
     #The West
-    elif i['properties']['ISO_A3'] in nation.The_West: nation.style_func('blue','blue', i,'The West',west)
+    elif i['properties']['ISO_A3'] in nation.The_West: 
+        y = time.time()
+        nation.style_func('blue','blue', i,'The West',west)
+        x = time.time()
+        func_time += x-y
     #The Orthodoxy
-    elif i['properties']['ISO_A3'] in nation.The_Orthodoxy: nation.style_func('purple','purple', i,'The Orthodoxy',orth)
+    elif i['properties']['ISO_A3'] in nation.The_Orthodoxy: 
+        y = time.time()
+        nation.style_func('purple','purple', i,'The Orthodoxy',orth)
+        x = time.time()
+        func_time += x-y
     #Cushite
-    elif  i['properties']['ISO_A3'] in nation.Cushite: nation.style_func('#376550','#376550', i,'Cushite',cush)
+    elif  i['properties']['ISO_A3'] in nation.Cushite: 
+        y = time.time()
+        nation.style_func('#376550','#376550', i,'Cushite',cush)
+        x = time.time()
+        func_time += x-y
     #West Africa
-    elif i['properties']['ISO_A3'] in nation.West_Africa: nation.style_func('#ff8c00','#ff8c00', i,'West Africa',westafr)
+    elif i['properties']['ISO_A3'] in nation.West_Africa: 
+        y = time.time()
+        nation.style_func('#ff8c00','#ff8c00', i,'West Africa',westafr)
+        x = time.time()
+        func_time += x-y
     #Bantoid
-    elif i['properties']['ISO_A3'] in nation.Bantoid:nation. style_func('#FBC490','#FBC490', i,'Bantu',bantu)
+    elif i['properties']['ISO_A3'] in nation.Bantoid:
+        y = time.time()
+        nation. style_func('#FBC490','#FBC490', i,'Bantu',bantu)
+        x = time.time()
+        func_time += x-y
     #South Africa
-    elif i['properties']['ISO_A3'] in nation.South_Africa: nation.style_func('black','black', i,'South Africa',southafr)
+    elif i['properties']['ISO_A3'] in nation.South_Africa: 
+        y = time.time()
+        nation.style_func('black','black', i,'South Africa',southafr)
+        x = time.time()
+        func_time += x-y
     #North Africa
-    elif i['properties']['ISO_A3'] in nation.North_Africa: nation.style_func('#00c300','#00c300', i,'North Africa',northafr)
+    elif i['properties']['ISO_A3'] in nation.North_Africa: 
+        y = time.time()
+        nation.style_func('#00c300','#00c300', i,'North Africa',northafr)
+        x = time.time()
+        func_time += x-y
     #Sudan
-    elif i['properties']['ISO_A3'] in nation.Sudan: nation.style_func('#FFCC00','#FFCC00', i,'Sudan',sud)
+    elif i['properties']['ISO_A3'] in nation.Sudan: 
+        y = time.time()
+        nation.style_func('#FFCC00','#FFCC00', i,'Sudan',sud)
+        x = time.time()
+        func_time += x-y
     #caucauses
-    elif i['properties']['ISO_A3'] in nation.Caucauses:nation.style_func('#75816b','#75816b', i,'Caucauses',cau)
-
+    elif i['properties']['ISO_A3'] in nation.Caucauses:
+        y = time.time()
+        nation.style_func('#75816b','#75816b', i,'Caucauses',cau)
+        x = time.time()
+        func_time += x-y
     #madagascar
-    elif i['properties']['ISO_A3'] in nation.Madagascar: nation.style_func('#800020','#800020', i,'Madagascar',mada)
+    elif i['properties']['ISO_A3'] in nation.Madagascar: 
+        y = time.time()
+        nation.style_func('#800020','#800020', i,'Madagascar',mada)
+        x = time.time()
+        func_time += x-y
     #melanesia
-    elif i['properties']['ISO_A3'] in nation.Melanesia: nation.style_func('#8da825','#8da825', i,'Melanesia',mel)
+    elif i['properties']['ISO_A3'] in nation.Melanesia: 
+        y = time.time()
+        nation.style_func('#8da825','#8da825', i,'Melanesia',mel)
+        x = time.time()
+        func_time += x-y
     #Micronesia
-    elif i['properties']['ISO_A3'] in nation.Micronesia: nation.style_func('#00008B','#00008B', i,'Micronesia',micro)
+    elif i['properties']['ISO_A3'] in nation.Micronesia: 
+        y = time.time()
+        nation.style_func('#00008B','#00008B', i,'Micronesia',micro)
+        x = time.time()
+        func_time += x-y
     #Polynesia
-    elif i['properties']['ISO_A3'] in nation.Polynesia: nation.style_func('#39FF14','#39FF14', i,'Polynesia',poly)
+    elif i['properties']['ISO_A3'] in nation.Polynesia:
+        y = time.time() 
+        nation.style_func('#39FF14','#39FF14', i,'Polynesia',poly)
+        x = time.time()
+        func_time += x-y
     #Himilayas
-    elif i['properties']['ISO_A3'] in nation.Himilayas: nation.style_func('#ACD5F3','#ACD5F3', i,'The Himilayas',him)
+    elif i['properties']['ISO_A3'] in nation.Himilayas: 
+        y = time.time()
+        nation.style_func('#ACD5F3','#ACD5F3', i,'The Himilayas',him)
+        x = time.time()
+        func_time += x-y
     #Euro-North American Settler-Colonies
-    elif i['properties']['ISO_A3'] in nation.Euro_North: nation.style_func('#000080','#000080', i,'Euro-North American Settler-Colonies',euro_am)
+    elif i['properties']['ISO_A3'] in nation.Euro_North: 
+        y = time.time()
+        nation.style_func('#000080','#000080', i,'Euro-North American Settler-Colonies',euro_am)
+        x = time.time()
+        func_time += x-y
     #Mexican-occupied Mexica
-    elif i['properties']['ISO_A3'] in nation.Mexica: nation.style_func('#125454','#125454', i,'Spanish-occupied Mexica',Mexica)
+    elif i['properties']['ISO_A3'] in nation.Mexica: 
+        y = time.time()
+        nation.style_func('#125454','#125454', i,'Spanish-occupied Mexica',Mexica)
+        x = time.time()
+        func_time += x-y
     #Portuguese-Occupied Tupi-Guarani
-    elif i['properties']['ISO_A3'] in nation.Tupi: nation.style_func('#F36196','#F36196', i,'Portuguese-Occupied Tupi-Guarani',Tupi)
+    elif i['properties']['ISO_A3'] in nation.Tupi: 
+        y = time.time()
+        nation.style_func('#F36196','#F36196', i,'Portuguese-Occupied Tupi-Guarani',Tupi)
+        x = time.time()
+        func_time += x-y
     #Spanish-Occupied Inca
-    elif i['properties']['ISO_A3'] in nation.Inca: nation.style_func('#C19A6B ','#C19A6B ', i,'Spanish-Occupied Inca',Inca)
+    elif i['properties']['ISO_A3'] in nation.Inca: 
+        y = time.time()
+        nation.style_func('#C19A6B ','#C19A6B ', i,'Spanish-Occupied Inca',Inca)
+        x = time.time()
+        func_time += x-y
     #Euro-Hispanic Occupied South-West America 
-    elif i['properties']['ISO_A3'] in nation.SW_Am: nation.style_func('#0d98ba ','#0d98ba ', i,'Euro-Hispanic Occupied South-West America ',Euro_Hispanic)
+    elif i['properties']['ISO_A3'] in nation.SW_Am: 
+        y = time.time()
+        nation.style_func('#0d98ba ','#0d98ba ', i,'Euro-Hispanic Occupied South-West America ',Euro_Hispanic)
+        x = time.time()
+        func_time += x-y
     #Gran_colombia
-    elif i['properties']['ISO_A3'] in nation.Gran_colombia: nation.style_func('#C49102 ','#C49102 ', i,'Gran_colombia',Gran_colombia)
+    elif i['properties']['ISO_A3'] in nation.Gran_colombia: 
+        y = time.time()
+        nation.style_func('#C49102 ','#C49102 ', i,'Grand colombia',Gran_colombia)
+        x = time.time()
+        func_time += x-y
     #Caribbean
-    elif i['properties']['ISO_A3'] in nation.Caribbean: nation.style_func('#00A36C ','#00A36C ', i,'Caribbean',Caribbean)
-
-    
-
+    elif i['properties']['ISO_A3'] in nation.Caribbean: 
+        y = time.time()
+        nation.style_func('#00A36C ','#00A36C ', i,'Caribbean',Caribbean)
+        x = time.time()
+        func_time += x-y
+#findings: style func is quite fast but the for loop itself may be overbearing
+end2 = time.time()
+print(f'function fetch runtime: {func_time}')
+print(f'breakpoint 2 runtime: {end2-start2} seconds')
 #folium.GeoJsonTooltip(fields=['TYPE', 'R_STATEFP', 'L_STATEFP']).add_to(US_Geojson)
 
 for map_name, url in maps.items():
@@ -228,6 +372,10 @@ folium.TileLayer('Stamen Toner').add_to(main_map)
 folium.TileLayer('Stamen Water Color').add_to(main_map)
 folium.TileLayer('cartodbpositron').add_to(main_map)
 folium.LayerControl().add_to(main_map)
+end1 = time.time()
+print(f'System runtime: {end1-start1} seconds')
+print(f'form_str func time: {func_time} seconds')
+print(f'API fetching {country_object.fetch_time}')
 main_map.save('output.html')
 #folium.GeoJson()
 
